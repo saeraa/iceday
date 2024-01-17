d7a1213036193783b3427c9c5486702cd2b4f103
Sara! <99484730+saeraa@users.noreply.github.com>
2024-01-17 15:37:23 +0100

    removed console.ts (#31)

--- console.ts
+++
@@ -1,7 +0,0 @@
-const originalWarn = console.error;
-console.error = function (message?: any, ...optionalParams: any[]) {
-  if (message.includes("defaultProps")) {
-    return;
-  }
-  originalWarn.apply(console, [message, ...optionalParams]);
-};
